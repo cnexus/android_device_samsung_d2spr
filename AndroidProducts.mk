d53722b79e690065d958af669671b3a54e819084
CNexus <cnexus0@gmail.com>
2013-10-29 19:30:01 -0700

    Adjust for omni build system

---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/full_d2spr.mk \
+	$(LOCAL_DIR)/omni_d2spr.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,3 @@
-add_lunch_combo cm_d2spr-eng
+add_lunch_combo full_d2spr-userdebug
+add_lunch_combo omni_d2spr-userdebug
 
